29c8290b3087e5f62479062efc87ca370bcb323d
Tassos Bareiss <tassosbareiss@gmail.com>
2017-05-15 19:54:24 -0400

    Add lyrics data compressor

--- js/util/data_cleaner.js
+++ js/util/data_cleaner.js
@@ -7,4 +7,25 @@ for (i=0; i < parseInt(lyrics.length); i++){
   curr.Lyrics = removePunctuation(curr.Lyrics);
 }
 
-jsonfile.writeFile('lyrics_no_punctuation.json', lyrics);
+const compressedLyrics = [];
+for (i=0; i < parseInt(lyrics.length); i++) {
+  const curr = lyrics[i];
+  const words = curr.Lyrics.split(' ');
+  curr.Lyrics = compress(words);
+  console.log(curr.Lyrics);
+}
+
+function compress(words) {
+  const dict = {};
+  words.forEach((wrd) => {
+    if (wrd === "") return;
+    if (dict[wrd]) {
+      dict[wrd]++;
+    } else {
+      dict[wrd] = 1;
+    }
+  });
+  return dict;
+}
+
+jsonfile.writeFile('compressed_data.json', lyrics);
