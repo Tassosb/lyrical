0732ffe0e084da81458e4db3a646d0c732a809a5
Tassosb <tassosbareiss@gmail.com>
2017-05-15 23:04:32 -0400

    Add DS_Store to gitignore and use uglify when bundling

--- webpack.config.js
+++ webpack.config.js
@@ -20,23 +20,23 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-map',
-  // plugins: [
-  // new webpack.optimize.UglifyJsPlugin({
-  //   comments: false, // remove comments
-  //   compress: {
-  //     unused: true,
-  //     dead_code: true, // big one--strip code that will never execute
-  //     warnings: false, // good for prod apps so users can't peek behind curtain
-  //     drop_debugger: true,
-  //     conditionals: true,
-  //     evaluate: true,
-  //     drop_console: true, // strips console statements
-  //     sequences: true,
-  //     booleans: true,
-  //     }
-  //   })
-  // ],
+  // devtool: 'source-map',
+  plugins: [
+  new webpack.optimize.UglifyJsPlugin({
+    comments: false, // remove comments
+    compress: {
+      unused: true,
+      dead_code: true, // big one--strip code that will never execute
+      warnings: false, // good for prod apps so users can't peek behind curtain
+      drop_debugger: true,
+      conditionals: true,
+      evaluate: true,
+      drop_console: true, // strips console statements
+      sequences: true,
+      booleans: true,
+      }
+    })
+  ],
   resolve: {
     extensions: ['*', '.js']
   }
